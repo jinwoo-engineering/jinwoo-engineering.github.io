fcd05cb22f536d736604080e0534711d88b75e46
thegicode <thegi.code@gmail.com>
2021-02-22 14:58:13 +0900

    Modifu rendering
    일괄로 적용하도록 수정

--- js/common.js
+++ js/common.js
@@ -1,37 +1,48 @@
 'use strict';
 
-const gnbTemplate = 
-`
-<h1 class="brand">진우엔지니어링</h1>
-<nav class="nav">
-	<ul class="nav-list">
-		<li class="nav-item">
-			<a href="../html/home.html" class="__link">HOME</a>
-		</li>
-		<li class="nav-item">
-			<a href="../html/about.html" class="__link">회사소개</a>
-		</li>
-		<li class="nav-item">
-			<a href="../html/status.html" class="__link">회사현황</a>
-		</li>
-		<li class="nav-item">
-			<a href="../html/business.html" class="__link">사업소개</a>
-		</li>
-		<li class="nav-item">
-			<a href="../html/sales.html" class="__link">매출계획</a>
-		</li>
-	</ul>
-</nav>`;
-const footerTemplate = `<h3>Footer</h3>`;
+var dom = document;
 
-// root.insertAdjacentHTML('afterbegin', gnbTemplate);
-gnb.className = "gnb";
-gnb.innerHTML = gnbTemplate;
+var tempateGnb = `
+	<h1 class="brand">진우엔지니어링</h1>
+	<nav class="nav">
+		<ul class="nav-list">
+			<li class="nav-item">
+				<a href="../html/home.html" class="__link">HOME</a>
+			</li>
+			<li class="nav-item">
+				<a href="../html/about.html" class="__link">회사소개</a>
+			</li>
+			<li class="nav-item">
+				<a href="../html/status.html" class="__link">회사현황</a>
+			</li>
+			<li class="nav-item">
+				<a href="../html/business.html" class="__link">사업소개</a>
+			</li>
+			<li class="nav-item">
+				<a href="../html/sales.html" class="__link">매출계획</a>
+			</li>
+		</ul>
+	</nav>`;
+	
+var template = {
+	gnb: tempateGnb,
+	footer: `<h3>Footer</h3>`,
+	gnbFix: tempateGnb
+};
 
-footer.className = "footer";
-footer.innerHTML = footerTemplate;
+var className = {
+	gnb: 'gnb',
+	footer: 'footer',
+	gnbFix: 'gnb gnbFix'
+};
 
-gnbFix.className = "gnb-fix";
-gnbFix.innerHTML = gnbTemplate;
+var components = dom.querySelectorAll('[data-component]');
+components.forEach( cp => {
+	var key = cp.dataset.component;
+	cp.className = className[key];
+	cp.innerHTML = template[key];
+});
+
+
 
 
