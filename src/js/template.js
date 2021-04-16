166b75c854b9c89babf3802636ca57dc96751d94
thegicode <thegi.code@gmail.com>
2021-04-16 16:43:31 +0900

    Modify footer

--- src/js/template.js
+++ src/js/template.js
@@ -39,7 +39,10 @@ Template.prototype.footer = function () {
         <div class="footer-inner">
             <div class="info">
                 <p class="address">경기도 동두천시 삼육사로 548번길 84 (상패동)</p>
-                <p class="tel">TEL : <a href="tel: 031-858-2430">031-858-2430</a></p>
+                <p class="tel">TEL : 031-858-2430 
+                    <a href="tel: 031-858-2430" class="__link">통화</a>
+                    <a href="tel: 031-858-2430" class="__link">문자</a>
+                </p>
                 <p class="tel">FAX : <a href="tel: 031-858-2429">031-858-2429</a></p>
                 <p class="email">EMAIL : <a href="mailto:jinwoo43705@naver.com">jinwoo43705@naver.com</a></p>
             </div>
