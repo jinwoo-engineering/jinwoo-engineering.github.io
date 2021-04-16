0ded49b0b5434f3e38203ab0a2699a2d5bb4b166
thegicode <thegi.code@gmail.com>
2021-04-16 16:38:30 +0900

    Modify footer

--- src/js/template.js
+++ src/js/template.js
@@ -39,8 +39,8 @@ Template.prototype.footer = function () {
         <div class="footer-inner">
             <div class="info">
                 <p class="address">경기도 동두천시 삼육사로 548번길 84 (상패동)</p>
-                <p class="tel">TEL : 031-858-2430</p>
-                <p class="tel">FAX : 031-858-2429</p>
+                <p class="tel">TEL : <a href="tel: 031-858-2430">031-858-2430</a></p>
+                <p class="tel">FAX : <a href="tel: 031-858-2429">031-858-2429</a></p>
                 <p class="email">EMAIL : <a href="mailto:jinwoo43705@naver.com">jinwoo43705@naver.com</a></p>
             </div>
             <div class="copyright">Copyright © JINWOO ENGINEERING. All rights reserved.</div>
