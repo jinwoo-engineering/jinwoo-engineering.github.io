4a118f88d59f82b5c5885f3ae0396ab78d6bdc62
thegicode <thegi.code@gmail.com>
2021-06-01 15:19:29 +0900

    Update

--- src/js/template.js
+++ src/js/template.js
@@ -2,13 +2,11 @@
     'use strict'
 
     function Template () {
-        const self = this
-
-        self.template = {
+        this.template = {
             gnb: function () {
                 return `
                     <h1 class="brand">진우엔지니어링</h1>
-                    <button type="button" class="toggle-button" data-gnb-button>
+                    <button type="button" class="toggle-button" data-gnb-button title="메뉴">
                         <span class="bars">
                             <span class="__bar"></span>
                             <span class="__bar"></span>
@@ -40,36 +38,30 @@
                     <div class="footer-inner">
                         <div class="info">
                             <p class="address">경기도 동두천시 삼육사로 548번길 84 (상패동)</p>
-                            <p class="tel">TEL : 031-858-2430 
-                                <a href="tel: 031-858-2430" class="__link">통화</a>
-                                <a href="sms: 031-858-2430" class="__link">문자</a>
-                            </p>
+                            <p class="tel">TEL : <a href="tel: 031-858-2430">031-858-2430</a></p>
                             <p class="tel">FAX : <a href="tel: 031-858-2429">031-858-2429</a></p>
                             <p class="email">EMAIL : <a href="mailto:jinwoo43705@naver.com">jinwoo43705@naver.com</a></p>
                         </div>
                         <div class="copyright">Copyright © JINWOO ENGINEERING. All rights reserved.</div>
                     </div>`
             },
-            gnbFix: function () {
-                return `<div class="gnbFix-inner">${self.template.gnb()}</div>`
-            }
+            gnbFix: () => `<div class="gnbFix-inner">${this.template.gnb()}</div>`
         }
     }
 
     Template.prototype.drawComponents = function (cpnts) {
-        const self = this
         const name = {
             gnb: 'gnb',
             footer: 'footer',
             gnbFix: 'gnb gnbFix'
         }
-        const draw = function (cpnt) {
+        const draw = cpnt => {
             const key = cpnt.getAttribute('data-component')
             cpnt.className = name[key]
             if (key === 'gnb' || key === 'gnbFix') {
                 cpnt.setAttribute('data-expanded', false)
             }
-            cpnt.innerHTML = self.template[key]()
+            cpnt.innerHTML = this.template[key]()
         }
         cpnts.forEach(function (cpnt) {
             draw(cpnt)
